c432e18e9e53fcac2e012cedcee3ed13927ae3f7
ambroseahawo <ahawoadeya995@gmail.com>
2022-05-04 15:02:55 +0300

    Initial Commit

---
+++ cards/lib/cards.ex
@@ -0,0 +1,53 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for `Cards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    # match values to suits
+    # list comprehension
+    # for loop in elixir
+
+    # solution one
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+
+    # List.flatten(cards)
+
+    # solution two
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+
+  end
+  # the number of arguments that a function accepts is known as arity
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  # Cards.deal(deck, 5) { *hand*, *deck* }
+
+end
