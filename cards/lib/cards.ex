18b4cbf3842c5f8ffeb13faf9babb8fbcc8c40ca
ambroseahawo <ahawoadeya995@gmail.com>
2022-05-09 22:00:00 +0300

    elixir-doctests

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,16 +1,11 @@
 defmodule Cards do
+
   @moduledoc """
-  Documentation for `Cards`.
+  This module provides methods for creating and handling a deck of cards.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
+    Returns a list of strings representing a deck of playing cards.
   """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
@@ -40,14 +35,78 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, hand) do
     Enum.member?(deck, hand)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
   # Cards.deal(deck, 5) { *hand*, *deck* }
+  # { hand, rest_of_deck } = { *hand*, *rest_of_deck* }
+  def pattern_matching do
+    color1 = ["red"]
+    "Color 1: #{color1}"
 
+    [color1] = ["red"]
+    "Color 1: #{color1}"
+
+    [color1, color2] = ["red", "blue"]
+    "Color 1: #{color1}, Color 2: #{color2}"
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    # method one
+    # {status, binary} = File.read(filename)
+
+    # avoid if statements in elixir,
+    # use case instead
+    # case status do
+    #   :ok -> :erlang.binary_to_term(binary)
+    #   :error -> "This file does not exist"
+    # end
+
+    # method two
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "This file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    # when using pipe operator, we do not need to do any assignment to a temporary variable.
+    # the result of each function is automaticaaly sent to the next funxtion in the chain
+    # the pipe operator demands the methods to take consistent first arguments to work properly.
+     Cards.create_deck()
+     |> Cards.shuffle()
+     |> Cards.deal(hand_size)
+  end
 end
