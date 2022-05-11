5a13684bebf4fb310477d60c0874beee813fa3d4
ambroseahawo <ahawoadeya995@gmail.com>
2022-05-11 21:42:59 +0300

    pacaman-rules

---
+++ elixir/pacman_rules/lib/pacman_rules.ex
@@ -0,0 +1,18 @@
+defmodule PacmanRules do
+  @moduledoc """
+  Documentation for `PacmanRules`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PacmanRules.hello()
+      :world
+
+  """
+  def eat_ghost?(power_pellet_active, is_touching_gost) do
+
+  end
+end
