7a5192eaad8232259efbf7893bfa1f2dd8d1c575
Ambrose Ahawo <ahawoadeya995@gmail.com>
2023-07-20 01:04:30 +0300

    Recursion - Print Digits

--- Notes.md
+++ Notes.md
@@ -5,3 +5,17 @@
 - Protocols
 - Processes and Concurrency
 - Macros
+
+lists vs tuples
+
+- recursion
+- function overloading
+
+
+
+c "file.exs"
+https://mermaid.js.org/intro/
+
+
+https://elixir-lang.org/docs.html
+https://hexdocs.pm/elixir/Kernel.html
---
+++ mix/tutorials/lib/recursion/print_digits.ex
@@ -0,0 +1,21 @@
+# executing in the ascending stage
+defmodule Tutorials.Recursion.PrintDigits do
+  # Base case
+  def up_to(0), do: 0
+
+  def up_to(num) do
+    IO.puts(num)
+    up_to(num - 1)
+  end
+end
+
+# executing in the return stage
+# defmodule Tutorials.Recursion.PrintDigits do
+#   # Base case
+#   def up_to(0), do: 0
+
+#   def up_to(num) do
+#     up_to(num - 1)
+#     IO.puts(num)
+#   end
+# end
