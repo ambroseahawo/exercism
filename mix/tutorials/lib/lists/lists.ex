7cc5cafbf775b0830e073f3a3cc1f33c81e1464b
Ambrose Ahawo <ahawoadeya995@gmail.com>
2023-09-05 20:01:19 +0300

    Lists

---
+++ mix/tutorials/lib/lists/lists.ex
@@ -0,0 +1,10 @@
+defmodule Tutorials.Lists do
+  def sum(nums), do: sum_tail_rec(nums)
+
+  def sum_simple([]), do: 0
+  def sum_simple([h | t]), do: h + sum_simple(t)
+
+  def sum_tail_rec(nums,acc \\ 0)
+  def sum_tail_rec([], acc), do: acc
+  def sum_tail_rec([h | t], acc), do: sum_tail_rec(t, acc + h)
+end
