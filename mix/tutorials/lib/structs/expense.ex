22a883ae0327228bf36b6bbc3644b392b603307b
Ambrose Ahawo <ahawoadeya995@gmail.com>
2023-09-10 21:10:03 +0300

    sample sexpense

--- mix/tutorials/lib/structs/expense.ex
+++ mix/tutorials/lib/structs/expense.ex
@@ -9,9 +9,18 @@ defmodule Tutorials.Structs.Expense do
     )
 
     @type t :: %Expense{
-        title: String.t(),
-        date:Date.t() | nil,
-        amount: number(),
-        store:String.t()     
-    }
+            title: String.t(),
+            date: Date.t() | nil,
+            amount: number(),
+            store: String.t()     
+        }
+    
+    def sample do
+        [
+            %Expense{title: "Grocery", date: ~D[2022-09-12], amount: 18.99, store: "Metro"},
+            %Expense{title: "Mobile", date: ~D[2022-10-18], amount: 55.65, store: "Bell"},
+            %Expense{title: "Jacket", date: ~D[2022-07-06], amount: 150.99, store: "Marks"},
+            %Expense{title: "Air Jordans", date: ~D[2022-10-30], amount: 799.99, store: "Nike"}
+        ]
+    end
 end
