c02e5f0c829a334e618c5b8d3e46c25f03e68f4b
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-09-19 23:27:19 +0300

    LanguageList delete head

--- elixir/language_list/lib/language_list.ex
+++ elixir/language_list/lib/language_list.ex
@@ -22,4 +22,10 @@ defmodule LanguageList do
   @spec delete([any],String.t()) :: [any]
   def delete(list, language), do: List.delete(list, language)
 
+  @doc """
+    delete first element
+  """
+  @spec remove_head([any]) :: [any]
+  def remove_head(list), do: List.delete(list, hd(list))
+
 end
