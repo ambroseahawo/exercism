4730e72b631b96da8a19b0ba34786fe0d3f190f2
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-09-19 23:23:02 +0300

    LanguageList add

--- elixir/language_list/lib/language_list.ex
+++ elixir/language_list/lib/language_list.ex
@@ -9,4 +9,11 @@ defmodule LanguageList do
   @spec new() :: List.t()
   def new(), do: []
 
+  @doc """
+    adds new item to list
+  """
+  @spec add([any], String.t()) :: [any]
+  def add(list, language), do: [language | list]  #fast
+  # def add(list, language), do: list++ [language]  #slow
+
 end
