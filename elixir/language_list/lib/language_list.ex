5a39ad3e385f2c96a7c0005f163e81abb1c534d6
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-09-19 22:56:50 +0300

    LanguageList initial commit

---
+++ elixir/language_list/lib/language_list.ex
@@ -0,0 +1,18 @@
+defmodule LanguageList do
+  @moduledoc """
+  Documentation for LanguageList.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LanguageList.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
