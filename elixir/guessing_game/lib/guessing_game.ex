b62f1c75c6709a9bb7cd150c6f17e445224c990c
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-09-20 20:37:18 +0300

    GuessingGame compare so close

--- elixir/guessing_game/lib/guessing_game.ex
+++ elixir/guessing_game/lib/guessing_game.ex
@@ -7,10 +7,12 @@ defmodule GuessingGame do
     return correct if right guess
     return Too high if guess is greater than secret number
     return Too low if guess is lower than secret number
+    return response to guesses that are close to the secret number.
   """
   @spec compare(number(), number()) :: String.t()
   def compare(secret_number, guess) when guess == secret_number, do: "Correct"
-  def compare(secret_number, guess) when guess > secret_number, do: "Too high"
-  def compare(secret_number, guess) when guess < secret_number, do: "Too low"
+  def compare(secret_number, guess) when guess > secret_number and ((guess - secret_number != 1) and (secret_number - guess != 1)), do: "Too high"
+  def compare(secret_number, guess) when guess < secret_number and ((guess - secret_number != 1) and (secret_number - guess != 1)), do: "Too low"
+  def compare(secret_number, guess) when (guess - secret_number == 1) or (secret_number - guess == 1), do: "So close"
 
 end
