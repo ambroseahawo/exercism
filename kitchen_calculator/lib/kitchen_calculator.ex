583e75bc4477d048217c8375691673cb674d81e1
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-09-21 18:45:12 +0300

    KitchenCalculator from milliliter

--- kitchen_calculator/lib/kitchen_calculator.ex
+++ kitchen_calculator/lib/kitchen_calculator.ex
@@ -19,4 +19,14 @@ defmodule KitchenCalculator do
   def to_milliliter({:tablespoon, _} = volume_pair), do: {:milliliter, (elem(volume_pair, 1)) * 15}
   def to_milliliter({:milliliter, _} = volume_pair), do: volume_pair
 
+  @doc """
+    convert milliliter volume-pair to another unit
+  """
+  @spec from_milliliter(Tuple.t(), Atom.t()) :: Tuple.t()
+  def from_milliliter(volume_pair, :cup = unit), do: {unit, (elem(volume_pair, 1)) / 240}
+  def from_milliliter(volume_pair, :fluid_ounce = unit), do: {unit, (elem(volume_pair, 1)) / 30}
+  def from_milliliter(volume_pair, :teaspoon = unit), do: {unit, (elem(volume_pair, 1)) / 5}
+  def from_milliliter(volume_pair, :tablespoon = unit), do: {unit, (elem(volume_pair, 1)) / 15}
+  def from_milliliter(volume_pair, :milliliter = unit), do: volume_pair
+
 end
