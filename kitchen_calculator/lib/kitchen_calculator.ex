2af114b249ef8c0fb94e4a4ef1deb3306a02a516
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-09-21 16:35:39 +0300

    KitchenCalculator initial commit

---
+++ kitchen_calculator/lib/kitchen_calculator.ex
@@ -0,0 +1,18 @@
+defmodule KitchenCalculator do
+  @moduledoc """
+  Documentation for KitchenCalculator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KitchenCalculator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
