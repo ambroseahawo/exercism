4fd4ea945f22435d1c2c8c96f8f6aebab0e945dd
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-09-22 23:08:06 +0300

    HighSchoolSweetheart initial

--- high_school_sweetheart/lib/high_school_sweetheart.ex
+++ high_school_sweetheart/lib/high_school_sweetheart.ex
@@ -9,4 +9,10 @@ defmodule HighSchoolSweetheart do
   @spec first_letter(String.t()) :: String.t()
   def first_letter(name), do: String.trim(name) |> String.at(0)
 
+  @doc """
+    format first letter as an initial
+  """
+  @spec initial(String.t()) :: String.t()
+  def initial(name), do: String.upcase(name) |> first_letter() |> Kernel.<>(".")
+
 end
