7bc3776664bc8c1862b0742b190a340eca99d18e
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-09-22 22:47:32 +0300

    HighSchoolSweetheart initial commit

---
+++ high_school_sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,18 @@
+defmodule HighSchoolSweetheart do
+  @moduledoc """
+  Documentation for HighSchoolSweetheart.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HighSchoolSweetheart.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
