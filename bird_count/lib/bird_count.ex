5172ddc58a5d2ea47ec2bc3196d07f42af03ae80
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-09-22 23:38:19 +0300

    BirdCount initial commit

---
+++ bird_count/lib/bird_count.ex
@@ -0,0 +1,18 @@
+defmodule BirdCount do
+  @moduledoc """
+  Documentation for BirdCount.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BirdCount.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
