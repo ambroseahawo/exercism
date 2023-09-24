7ccce2a571524e53d3a6f854b315c34c48816fae
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-09-24 16:57:14 +0300

    BirdCount increment day count

--- bird_count/lib/bird_count.ex
+++ bird_count/lib/bird_count.ex
@@ -10,4 +10,13 @@ defmodule BirdCount do
   def today([]), do: nil
   def today(list), do: hd(list)
 
+  @doc """
+    take a list of daily bird counts and increment the today's count by 1
+  """
+  @spec increment_day_count([number()]) :: [number()]
+  def increment_day_count([]), do: [1]
+  def increment_day_count(list) do
+    Enum.map(list, fn list_item -> if hd(list) == list_item do list_item + 1 else list_item end end)
+  end
+
 end
