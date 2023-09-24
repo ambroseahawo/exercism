4a0832bc79827fef531edd3e60561619f5d1367e
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-09-24 21:59:55 +0300

    HighScore initial commit

---
+++ high_score/lib/high_score.ex
@@ -0,0 +1,18 @@
+defmodule HighScore do
+  @moduledoc """
+  Documentation for HighScore.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HighScore.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
