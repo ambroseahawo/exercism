6631953735234ec92c668e7e92728c999e45f0ab
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-09-24 22:31:09 +0300

    HighScore remove item from map

--- high_score/lib/high_score.ex
+++ high_score/lib/high_score.ex
@@ -15,4 +15,10 @@ defmodule HighScore do
   @spec add_player(Map.t(), String.t(), number()) :: Map.t()
   def add_player(scores, name, score \\ 0), do: Map.put(scores, name, score)
 
+  @doc """
+    remove item from map
+  """
+  @spec remove_player(Map.t(), String.t()) :: Map.t()
+  def remove_player(scores, name), do: Map.delete(scores, name)
+
 end
