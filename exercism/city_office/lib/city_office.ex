fe463fb6bbdfb995bc01559e39cb1b33c971e2db
Ambrose Ahawo <ambroseahawo@gmail.com>
2023-12-10 23:23:45 +0300

    City Office initial commit

---
+++ exercism/city_office/lib/city_office.ex
@@ -0,0 +1,18 @@
+defmodule CityOffice do
+  @moduledoc """
+  Documentation for `CityOffice`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CityOffice.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
