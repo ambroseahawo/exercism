68d214e4e1c1bb6fa06aba3a1ca47e44aeac0e7a
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-12-16 12:15:54 +0300

    Implement name-badge with cond/1

--- elixir/name-badge/test/name_badge_test.exs
+++ elixir/name-badge/test/name_badge_test.exs
@@ -8,10 +8,18 @@ defmodule NameBadgeTest do
       assert NameBadge.print(455, "Mary M. Brown", "MARKETING") ==
                "[455] - Mary M. Brown - MARKETING"
     end
+    test "prints the employee badge with full data using cond" do
+      assert NameBadge.print_with_cond(455, "Mary M. Brown", "MARKETING") ==
+               "[455] - Mary M. Brown - MARKETING"
+    end
 
     @tag task_id: 1
-    test "uppercases the department" do
+    test "does uppercase on department" do
       assert NameBadge.print(89, "Jack McGregor", "Procurement") ==
+               "[89] - Jack McGregor - PROCUREMENT"
+    end
+    test "does uppercase on department using cond" do
+      assert NameBadge.print_with_cond(89, "Jack McGregor", "Procurement") ==
                "[89] - Jack McGregor - PROCUREMENT"
     end
 
@@ -19,15 +27,24 @@ defmodule NameBadgeTest do
     test "prints the employee badge without id" do
       assert NameBadge.print(nil, "Barbara White", "Security") == "Barbara White - SECURITY"
     end
+    test "prints the employee badge without id using cond" do
+      assert NameBadge.print_with_cond(nil, "Barbara White", "Security") == "Barbara White - SECURITY"
+    end
 
     @tag task_id: 3
     test "prints the owner badge" do
       assert NameBadge.print(1, "Anna Johnson", nil) == "[1] - Anna Johnson - OWNER"
     end
+    test "prints the owner badge using cond" do
+      assert NameBadge.print_with_cond(1, "Anna Johnson", nil) == "[1] - Anna Johnson - OWNER"
+    end
 
     @tag task_id: 3
     test "prints the owner badge without id" do
       assert NameBadge.print(nil, "Stephen Dann", nil) == "Stephen Dann - OWNER"
+    end
+    test "prints the owner badge without id using cond" do
+      assert NameBadge.print_with_cond(nil, "Stephen Dann", nil) == "Stephen Dann - OWNER"
     end
   end
 end
